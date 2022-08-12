33879c9a60a2ae4f540bde30f3b9c536b12ffa47
jacare-camabox <ramonlindo2311@gmail.com>
2022-08-11 22:52:20 -0300

    feat: implement page` Bingo `

--- src/pages/Bingo/index.tsx
+++ src/pages/Bingo/index.tsx
@@ -1,11 +1,33 @@
 import React from "react";
 import "./index.css";
+import { useParams } from "react-router-dom";
 
 const Bingo = () => {
+  let { nickname } = useParams();
+
+  console.log(nickname);
+
   return (
     <>
+      <header>participants dddd</header>
       <body>
-        <div> Mega Bingão do G5</div>;
+        <div className="participants">
+          <li>{nickname}</li>
+          <li>{nickname}</li>
+          <li>{nickname}</li>
+        </div>
+        <div className="form">
+          <form action="">
+            <div className="chat">conversa</div>
+            <input type="text" />
+            <button className="button" type="submit">
+              Send
+            </button>
+          </form>
+        </div>
+        <div className="your-cards">
+          <div className="card">card</div>
+        </div>
       </body>
     </>
   );
