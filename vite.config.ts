0ffa9da5b8b06cf325dcdc32031be6c6090d365d
jacare-camabox <ramonlindo2311@gmail.com>
2022-08-31 21:14:02 -0300

    config deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,14 @@
-import { defineConfig, loadEnv } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig, loadEnv } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default ({ mode }) => {
-  process.env = Object.assign(process.env, loadEnv(mode, process.cwd(), ''));
+  process.env = Object.assign(process.env, loadEnv(mode, process.cwd(), ""));
 
   return defineConfig({
     plugins: [react()],
     server: {
-      port: process.env.PORT,
+      port: +process.env.PORT,
     },
   });
 };
