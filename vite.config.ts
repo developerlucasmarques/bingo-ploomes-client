6f0979d8d118936bcf1a898b8d1653dccd7a78ae
Lucas Marques <76230078+mlucasdev@users.noreply.github.com>
2022-08-31 21:04:26 -0300

    update viteconfig.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,15 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: process.env.PORT,
-  },
-});
+export default ({ mode }) => {
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
+
+  return defineConfig({
+    plugins: [react()],
+
+    server: {
+      port: process.env.PORT,
+    },
+  });
+};
