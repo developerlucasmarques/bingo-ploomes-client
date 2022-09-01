3d7e0c2bc39dfcd4697055f86085685f95267720
Lucas Marques <76230078+mlucasdev@users.noreply.github.com>
2022-08-31 21:26:41 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,7 @@
-import { defineConfig, loadEnv } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default ({ mode }) => {
-  process.env = Object.assign(process.env, loadEnv(mode, process.cwd(), ""));
-
-  return defineConfig({
-    plugins: [react()],
-    server: {
-      port: +process.env.PORT,
-    },
-  });
-};
+export default defineConfig({
+  plugins: [react()]
+})
